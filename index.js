94c9fba3b9a05b9d1b47ec2b58169d483bb44f81
Julia <54505247+vongrivatch@users.noreply.github.com>
2022-11-27 19:27:11 +0100

    Update index.js
    dodanie komentarzy do kodu

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ var path = require('path');
 
 const app = express();
 
+// funkja odpowiedzialna za wyswietlanie info o poprawnym logowaniu lub niepoprawnym
 function authentication(req, res, next) {
 	var authheader = req.headers.authorization;
 	console.log(req.headers);
-
+	//niepoprawne logowanie
 	if (!authheader) {
 		var err = new Error('You are not authenticated!');
 		res.setHeader('WWW-Authenticate', 'Basic');
 		err.status = 401;
 		return next(err)
 	}
-
 	var auth = new Buffer.from(authheader.split(' ')[1],
 	'base64').toString().split(':');
 	var user = auth[0];
 	var pass = auth[1];
-
-	if (user == '' && pass == '') {
-
-		// If Authorized user
+	// prawidlowy login i haslo uzytkownika
+	if (user == 'user' && pass == 'password') {
+		// poprawne logowanie
 		next();
 	} else {
 		var err = new Error('You are not authenticated!');
@@ -33,11 +32,11 @@ function authentication(req, res, next) {
 
 }
 
-// First step is the authentication of the client
+// Pierwszy krok to uwierzytelnianie klienta
 app.use(authentication)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Server setup
+// Server setup tutaj podany adres lokalny
 app.listen((3000), () => {
 	console.log("Server is Running");
 })
